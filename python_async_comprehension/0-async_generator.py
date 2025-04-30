f473bb0d90e8b3429510dc6bd29959967f45cd82
nyacho04 <dnachod83@gmail.com>
2025-04-30 11:15:10 -0300

    async generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """notedddddddddddd"""
 import asyncio
 import random
+import typing
 
 
-async def async_generator():
+async def async_generator() -> typing.AsyncGenerator[float, None]:
     """notedddddddddddddddddd"""
     for _ in range(10):
         await asyncio.sleep(1)
