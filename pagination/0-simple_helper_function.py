3d814146416c6d22ba176200d51af56d153f355f
nyacho04 <dnachod83@gmail.com>
2025-04-30 14:48:32 -0300

    pagination

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+module module module module module
+"""
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """returns a tuple containing the start and end index for pagination and
+    calculate the start and end index."""
+    index1 = (page - 1) * page_size
+    index2 = page * page_size
+    return index1, index2
